4cb8a37563de670fc8767fd7cedc39189a95d51f
Renan de Lima Gomes <renanlg@ciandt.com>
2022-06-08 11:06:26 -0300

    Testes de software - JUnit - Parte 2 - 0. Password

---
+++ 0x02/password/src/Person.java
@@ -0,0 +1,31 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Person {
+
+    public boolean checkUser(String user) {
+        Pattern specificCharacters = Pattern.compile("[^a-z0-9 ]", Pattern.CASE_INSENSITIVE);
+        Matcher findSpecificCharacters = specificCharacters.matcher(user);
+        return user.length() >= 8 || !findSpecificCharacters.find();
+    }
+
+    public boolean checkPassword(String password) {
+        // Has UpperCase Char
+        Pattern upperCaseCharacters = Pattern.compile("[A-Z]");
+        Matcher findUpperCaseCharacters = upperCaseCharacters.matcher(password);
+
+        Pattern onlyNumbers = Pattern.compile("[0-9]");
+        Matcher findOnlyNumbers = onlyNumbers.matcher(password);
+
+        Pattern specialCharacters = Pattern.compile("[^a-z0-9 ]", Pattern.CASE_INSENSITIVE);
+        Matcher findSpecialCharacters = specialCharacters.matcher(password);
+
+        if (password.length() >= 8 &&
+            findUpperCaseCharacters.find() &&
+            findOnlyNumbers.find() &&
+            findSpecialCharacters.find())
+            return true;
+
+        return false;
+    }
+}
---
+++ 0x02/password/src/PersonTest.java
@@ -0,0 +1,54 @@
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import static org.junit.jupiter.api.Assertions.*;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class PersonTest {
+    private Person p;
+
+    @BeforeAll
+    void setup(){
+         p = new Person();
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"PaulMcCartney2", "NeilArms2"})
+    void check_user_valid(String user){
+        assertTrue(p.checkUser(user));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"Paul#McCartney", "Neil@Arms"})
+    void check_user_not_valid(String user){
+        assertTrue(p.checkUser(user));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"123456789", "#$%1234"})
+    void does_not_have_letters(String password){
+        assertFalse(p.checkPassword(password));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"Abcabcdefgh@", "#hbtn@%tc"})
+    void does_not_have_numbers(String password){
+        assertFalse(p.checkPassword(password));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"Abc@123", "12$@hbt"})
+    void does_not_have_eight_chars(String password){
+        assertFalse(p.checkPassword(password));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"abC123456$", "Hbtn@1234", "Betty@1#2", "Hbtn@123"})
+    void check_password_valid(String password){
+        assertTrue(p.checkPassword(password));
+    }
+
+
+
+}
